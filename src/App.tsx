a343b987a58c9d42b6a6d8d179d55fdafc58ca65
stevebarakat <stevebarakat@gmail.com>
2025-10-16 23:37:32 -0400

    top

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,20 @@ function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <svg viewBox="0 0 16 16">
+    <svg className="mouth" viewBox="0 0 16 16">
+      <line className="tooth" x1="3" y1="2.5" x2="3" y2="3.5" />
+      <line className="tooth" x1="5" y1="2" x2="5" y2="4" />
+      <line className="tooth" x1="7" y1="2" x2="7" y2="5" />
+      <line className="tooth" x1="9" y1="2" x2="9" y2="5" />
+      <line className="tooth" x1="11" y1="2" x2="11" y2="4" />
+      <line className="tooth" x1="13" y1="2.5" x2="13" y2="3.5" />
       <path
+        className="lips"
         d="
-      M 2,2
-      A 4,4 0 0 0 14,2
-      L 2,2
-    "
+    M 2,2
+    A 4,4 0 0 0 14,2
+    L 2,2
+  "
       />
     </svg>
   );

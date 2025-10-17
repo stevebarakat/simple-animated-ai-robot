9dc65ae1101a942bc3814d770173d590ca325f5a
stevebarakat <stevebarakat@gmail.com>
2025-10-16 23:48:15 -0400

    co

--- src/App.tsx
+++ src/App.tsx
@@ -6,12 +6,16 @@ function App() {
 
   return (
     <svg className="mouth" viewBox="0 0 16 16">
-      <line className="tooth" x1="3" y1="2.5" x2="3" y2="3.5" />
-      <line className="tooth" x1="5" y1="2" x2="5" y2="4" />
+      <line className="tooth" x1="3" y1="2.5" x2="3" y2="4" />
+      <line className="tooth" x1="5" y1="2" x2="5" y2="4.5" />
       <line className="tooth" x1="7" y1="2" x2="7" y2="5" />
       <line className="tooth" x1="9" y1="2" x2="9" y2="5" />
-      <line className="tooth" x1="11" y1="2" x2="11" y2="4" />
-      <line className="tooth" x1="13" y1="2.5" x2="13" y2="3.5" />
+      <line className="tooth" x1="11" y1="2" x2="11" y2="4.5" />
+      <line className="tooth" x1="13" y1="2.5" x2="13" y2="4" />
+      <line className="tooth" x1="5" y1="5" x2="5" y2="7" />
+      <line className="tooth" x1="7" y1="5.5" x2="7" y2="7.5" />
+      <line className="tooth" x1="9" y1="5.5" x2="9" y2="7.5" />
+      <line className="tooth" x1="11" y1="5" x2="11" y2="7" />
       <path
         className="lips"
         d="

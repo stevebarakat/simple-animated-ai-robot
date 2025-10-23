ca608ee89f56121b2bd7de8fb9d73ae904aa48c8
stevebarakat <stevebarakat@gmail.com>
2025-10-22 22:48:16 -0400

    ci

--- src/App.tsx
+++ src/App.tsx
@@ -21,18 +21,18 @@ function App() {
       />
       {/* top teeth */}
       <line className="tooth" x1="5.1" y1="10.3" x2="5.1" y2="10.8" />
-      <line className="tooth" x1="6.3" y1="10.3" x2="6.3" y2="11" />
+      <line className="tooth" x1="6.2" y1="10.3" x2="6.2" y2="11" />
       <line className="tooth" x1="7.4" y1="10.3" x2="7.4" y2="11.2" />
       <line className="tooth" x1="8.6" y1="10.3" x2="8.6" y2="11.2" />
-      <line className="tooth" x1="9.7" y1="10.3" x2="9.7" y2="11" />
+      <line className="tooth" x1="9.8" y1="10.3" x2="9.8" y2="11" />
       <line className="tooth" x1="10.9" y1="10.3" x2="10.9" y2="10.8" />
 
       {/* bottom teeth */}
       <line className="tooth" x1="5.1" y1="11.1" x2="5.1" y2="11.9" />
-      <line className="tooth" x1="6.3" y1="11.3" x2="6.3" y2="12.1" />
+      <line className="tooth" x1="6.2" y1="11.3" x2="6.2" y2="12.1" />
       <line className="tooth" x1="7.4" y1="11.5" y2="12.3" x2="7.4" />
       <line className="tooth" x1="8.6" y1="11.5" x2="8.6" y2="12.7" />
-      <line className="tooth" x1="9.7" y1="11.3" x2="9.7" y2="12.7" />
+      <line className="tooth" x1="9.8" y1="11.3" x2="9.8" y2="12.7" />
       <line className="tooth" x1="10.9" y1="11.1" x2="10.9" y2="12.1" />
       <path
         className="lips"
